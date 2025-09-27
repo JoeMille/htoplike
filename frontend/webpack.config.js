95fc366c2e041250efd0e78ddaf7b9133d2ac545
Joe M. <chefjoemiller1992@gmail.com>
2025-09-27 21:00:06 +0100

    set up webpack config fr frontend. end sesh

---
+++ frontend/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+
+    devServer: {
+        port: 3000,
+        hot: true,
+        historyApiFallback: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+        }),
+    ],
+
+    mode: 'development',
+};
